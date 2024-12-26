c88e8e984b653664e62b79465837d7299c2923d3
Zoya <zoyarehman233@gmail.com>
2024-12-27 01:00:37 +0530

    password generator code

---
+++ code.py
@@ -0,0 +1,12 @@
+#password generator
+import random
+import string
+pass_len = int(input("enter the length of your password you want : "))
+char_values = string.ascii_letters + string.digits
+
+password = ""
+for i in range(pass_len):
+    passw = random.choice(char_values)
+    password += passw
+
+print(password)
